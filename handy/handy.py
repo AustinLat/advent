00274b3fc5453a48f8d3231f09a41e182ba6ccce
AustinLat <alaturno@gmail.com>
2021-01-08 16:51:39 -0800

    comit

--- handy/handy.py
+++ handy/handy.py
@@ -1,22 +1,21 @@
 def handy():
     with open("input.txt", "r") as r:
         all_lines = r.read()
-    # print(all_lines.splitlines())
+
     bags_for_gold = []
     for rule in all_lines.splitlines():
         rule = rule.split("bags contain")
         if "shiny gold" in rule[1]:
             bags_for_gold.append(rule[0])
 
+    bag_set = set(bags_for_gold)
     while True:
-        bag_set = set(bags_for_gold)
         bags_for_gold = list(bag_set)
         for rule in all_lines.splitlines():
             rule = rule.split("bags contain")
             for bag in bags_for_gold:
                 if bag in rule[1]:
                     bag_set.add(rule[0])
-
         if len(bag_set) == len(bags_for_gold):
             break
 
