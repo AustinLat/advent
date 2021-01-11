848901e1762f32001e873bdd6e6327f266b94901
AustinLat <alaturno@gmail.com>
2021-01-10 16:38:28 -0800

    second handy

---
+++ handy/handy_.py
@@ -0,0 +1,37 @@
+def handy():
+    with open("input.txt", "r") as r:
+        all_lines = r.read()
+    #Split each rule, and find bags that can hold gold.
+    bags_for_gold = []
+    for rule in all_lines.splitlines():
+        rule = rule.split("bags contain")
+        if "shiny gold" in rule[0]:
+            # print(rule)
+            bags = rule[1].split(",")
+            for i in bags:
+                bags_for_gold.append(i.strip())
+    print(bags_for_gold)
+
+            # bag_set = set(bags_for_gold)
+
+    #Turn updated bag_set back to list for looping purposes.
+    #loop through list, adding more bags that can eventually hold
+    #gold, adding them to the set above to remove duplicates.
+    #
+    # while True:
+    #     bags_for_gold = list(bag_set)
+    #     for rule in all_lines.splitlines():
+    #         rule = rule.split("bags contain")
+    #         for bag in bags_for_gold:
+    #             if bag in rule[1]:
+    #                 bag_set.add(rule[0])
+
+        #checking if bag_set constant equals the length of the
+        #list used for looping.  If so, that means no new items were added
+        #to the set, and while exits.
+
+        # if len(bag_set) == len(bags_for_gold):
+        #     break
+
+    # print(len(bag_set))
+handy()
