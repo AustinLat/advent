bbbca6330fd57da3e6c6352444a9643bc6555c88
AustinLat <alaturno@gmail.com>
2021-01-11 18:42:54 -0800

    so close

--- handy/handy_.py
+++ handy/handy_.py
@@ -1,37 +1,52 @@
 def handy():
     with open("input.txt", "r") as r:
         all_lines = r.read()
-    #Split each rule, and find bags that can hold gold.
-    bags_for_gold = []
+
+    bag_count = 0
+    bags_for_gold = {}
     for rule in all_lines.splitlines():
         rule = rule.split("bags contain")
         if "shiny gold" in rule[0]:
-            # print(rule)
-            bags = rule[1].split(",")
-            for i in bags:
-                bags_for_gold.append(i.strip())
-    print(bags_for_gold)
+            bags = rule[1].strip().split(", ")
+            for bag in bags:
+                if bag[-1] == ".":
+                    bag = bag[:-1]
+                bag = bag.rsplit(' ', 1)[0]
+                bag = bag.split(" ", 1)
+                bags_for_gold[bag[1]] = int(bag[0])
+                bag_count += int(bags_for_gold[bag[1]])
 
-            # bag_set = set(bags_for_gold)
+    while bags_for_gold:
 
-    #Turn updated bag_set back to list for looping purposes.
-    #loop through list, adding more bags that can eventually hold
-    #gold, adding them to the set above to remove duplicates.
-    #
-    # while True:
-    #     bags_for_gold = list(bag_set)
-    #     for rule in all_lines.splitlines():
-    #         rule = rule.split("bags contain")
-    #         for bag in bags_for_gold:
-    #             if bag in rule[1]:
-    #                 bag_set.add(rule[0])
+        for rule in all_lines.splitlines():
+            rule = rule.split("bags contain")
+            new_bags = {}
+            old_bags = {}
+            for bag in bags_for_gold:
+                if bag in rule[0]:
+                    bags = rule[1].strip().split(", ")
+                    for new_bag in bags:
+                        if new_bag[-1] == ".":
+                            new_bag = new_bag[:-1]
+                        new_bag = new_bag.rsplit(' ', 1)[0]
+                        new_bag = new_bag.split(" ", 1)
+                        if new_bag[0] == "no":
+                            old_bags[bag] = bags_for_gold[bag]
+                            continue
+                        else:
+                            new_bags[new_bag[1]] = int(new_bag[0])
+                            old_bags[bag] = bags_for_gold[bag]
 
-        #checking if bag_set constant equals the length of the
-        #list used for looping.  If so, that means no new items were added
-        #to the set, and while exits.
+            print(bag_count)
+            bag_count += sum(new_bags.values()) * int(bags_for_gold[bag])
+            for old in old_bags:
+                del bags_for_gold[old]
+            for new in new_bags:
+                bags_for_gold[new] = new_bags[new]
 
-        # if len(bag_set) == len(bags_for_gold):
-        #     break
 
-    # print(len(bag_set))
+    # print(bags_for_gold)
+    # print(bag_count)
+
+
 handy()
