613173f83a98da60a8515282d0b4065701c037cd
Austin <alaturno@gmail.com>
2021-01-13 17:19:29 -0800

    handheld work

--- handheld/handheld.py
+++ handheld/handheld.py
@@ -1,50 +1,56 @@
-with open("input.txt", "r") as data:
-    data = data.read().splitlines()
-    data = [line for line in data]
+# with open("input.txt", "r") as data:
+#     data = data.read().splitlines()
+#     data = [line for line in data]
+
+data = """nop +0
+    acc +1
+    jmp +4
+    acc +3
+    jmp -3
+    acc -99
+    acc +1
+    jmp -4
+    acc +6"""
+data = data.splitlines()
+data = [line for line in data]
 
 visited = []
 index = 0
 aac = 0
-
+command = data[index]
 while True:
-
-    if index + 1 == len(data):
-        break
     visited.append(index)
+    if index  == len(data):
+        break
+
+    if command.split(" ")[0] == "acc":
+        aac += int(command.split(" ")[1])
+        index = index + 1
+
+    if command.split(" ")[0] == "jmp":
+        if "+" in command.split(" ")[1]:
+            number = int(command.split(" ")[1][1:])
+        else:
+            number = int(command.split(" ")[1])
+        index = index + number
+
+    if command.split(" ")[0] == "nop":
+        index = index + 1
+
+
+
+    if index in visited:
+        index = visited[-2]
+
+        if command.split(" ")[0] == "jmp":
+            command = "nop" + " " + command.split(" ")[1]
+        else:
+            command = "jmp" + " " + command.split(" ")[1]
 
     command = data[index]
 
-    if index in visited:
-        if command.split(" ")[0] == "jmp":
-            command.split(" ")[0] = "nop"
-            index = index + 1
-
-        if command.split(" ")[0] == "nop":
-            command.split(" ")[0] = "jmp"
-            if "+" in command.split(" ")[1]:
-                number = int(command.split(" ")[1][1:])
-            else:
-                number = int(command.split(" ")[1])
-            index = index + number
-
-    else:
-        if command.split(" ")[0] == "acc":
-            aac += int(command.split(" ")[1])
-            index = index + 1
-
-        if command.split(" ")[0] == "jmp":
-            if "+" in command.split(" ")[1]:
-                number = int(command.split(" ")[1][1:])
-            else:
-                number = int(command.split(" ")[1])
-            index = index + number
-
-        if command.split(" ")[0] == "nop":
-            index = index + 1
 
 
-# print(visited)
-# for i in range(len(visited)):
-    # print(data[i])
-    # print(i)
-print(aac)
+    print(visited)
+    print(index)
+    print(aac)
