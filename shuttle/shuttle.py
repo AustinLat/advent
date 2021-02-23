24e6eafbfa40f03f704ed89f63325c4c590645c5
AustinLat <alaturno@gmail.com>
2021-02-22 19:19:54 -0800

    part one of day 13 completed

--- shuttle/shuttle.py
+++ shuttle/shuttle.py
@@ -1,7 +1,20 @@
 def shuttle():
     with open("input.txt", "r") as r:
         data = r.read()
-    print(data)
+        data = data.splitlines()
+    time = int(data[0])
+    start_t = time
+    buses = data[1].split(",")
+    buses[:] = [int(i) for i in buses if i != "x"]
+    
+    
+         
+    for bus in buses:
+        if time % bus == 0:
+            wait_time = time - start_t
+            print(wait_time * bus)
+            break
+        time += 1
 
 if __name__ == "__main__":
     shuttle()
