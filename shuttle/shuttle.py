59c648039dfac7422805eb7464b749f7c354cdf5
Austin <alaturno@gmail.com>
2021-02-22 21:06:37 -0800

    might have solution to day 13, seconds half.  Need to increase speed to find out.

--- shuttle/shuttle.py
+++ shuttle/shuttle.py
@@ -1,20 +1,27 @@
 def shuttle():
-    with open("input.txt", "r") as r:
-        data = r.read()
-        data = data.splitlines()
-    time = int(data[0])
-    start_t = time
-    buses = data[1].split(",")
-    buses[:] = [int(i) for i in buses if i != "x"]
+	with open("input.txt", "r") as r:
+		data = r.read()
+		data = data.splitlines()
+	time = 0
+	buses = data[1].split(",")
+	#buses[:] = [int(i) for i in buses if i != "x"]
     
     
-         
-    for bus in buses:
-        if time % bus == 0:
-            wait_time = time - start_t
-            print(wait_time * bus)
-            break
-        time += 1
+	while True:
+		time = time + int(buses[0])
+		if time % int(buses[0]) == 0:
+
+			for index, bus in enumerate(buses):
+				time = time + index
+				if buses[index] == "x":
+					continue	
+				if time % int(bus) == 0:
+					if index == len(buses) - 1 and time % int(buses[0]) == 0:
+						print(time)
+					continue
+				break
+		
+		print(time)
 
 if __name__ == "__main__":
     shuttle()
