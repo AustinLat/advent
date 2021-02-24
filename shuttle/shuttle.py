5a1024ef4b94a4ae8097a300d0443cef1fca8ef2
Austin <alaturno@gmail.com>
2021-02-23 21:40:13 -0800

    numpy may not be the solution

--- shuttle/shuttle.py
+++ shuttle/shuttle.py
@@ -1,28 +1,34 @@
 import numpy as np
+
+
 def shuttle():
 	with open("input.txt", "r") as r:
 		data = r.read()
 		data = data.splitlines()
 	time = 0
-	buses = data[1].split(",")
+	#buses = data[1].split(",")
 	#buses[:] = [int(i) for i in buses if i != "x"]
-        np_buses = np.array(buses)            
-    
+	buses = np.array(data[1].split(","))
+
 	while True:
+	#for t in np.nditer(time):
 		time = time + int(buses[0])
 		if time % int(buses[0]) == 0:
 
-			for index, bus in enumerate(buses):
-				time = time + index
+			for index, bus in enumerate(np.nditer(buses)):
+
+		#	for index, bus in enumerate(buses):
+				temp_time = time + index
 				if buses[index] == "x":
 					continue	
-				if time % int(bus) == 0:
-					if index == len(buses) - 1 and time % int(buses[0]) == 0:
-						print(time)
+				if temp_time % int(bus) == 0:
+					if index == len(buses) - 1 and temp_time % int(buses[0]) == 0:
+						return t
+						
 					continue
 				break
 		
 		print(time)
 
 if __name__ == "__main__":
-    shuttle()
+    print(shuttle())
