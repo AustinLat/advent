43a003131f22ce23abab21f4031e054e9c8c2d38
Austin <alaturno@gmail.com>
2021-03-09 09:09:32 -0800

    day 15 part 1 solved

--- recitation/recitation.py
+++ recitation/recitation.py
@@ -1,14 +1,15 @@
 def recitation(sequence):
 	number_list = sequence
-	while len(number_list) < 10:
-		print(number_list)
+	while len(number_list) < 2020:
 		if number_list[-1] not in number_list[:-1]:
 			number_list.append(0)
 		else:
-			last_occurance = len(number_list) - 1 - number_list[-2::-1].index(number_list[-1]) 
-			number_list.append(len(number_list)-(last_occurance+1))
-	print(number_list)
+			last_occurance = len(number_list) -1- number_list[-2::-1].index(number_list[-1])	
+			number_list.append(len(number_list)-(last_occurance))
+		print(number_list)
+	print(number_list[-1])
+
 
 if __name__=="__main__":
-	recitation([0,3,6])
+	recitation([10,16,6,0,1,17])
 	
