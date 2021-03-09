f7d17ad61a97daf10ae69cf2876cc5f44914951c
Austin <alaturno@gmail.com>
2021-03-09 14:56:48 -0800

    working on dasy 15 part 2.  Need to create a generator.

--- recitation/recitation.py
+++ recitation/recitation.py
@@ -1,15 +1,26 @@
-def recitation(sequence):
-	number_list = sequence
-	while len(number_list) < 2020:
-		if number_list[-1] not in number_list[:-1]:
-			number_list.append(0)
-		else:
-			last_occurance = len(number_list) -1- number_list[-2::-1].index(number_list[-1])	
-			number_list.append(len(number_list)-(last_occurance))
-		print(number_list)
-	print(number_list[-1])
+class big_gen():
+	
+	def recitation(sequence):
+		self.number_list = sequence
+		while len(number_list) < 2020:
+		#while True:
+			if self.number_list[-1] not in self.number_list[:-1]:
+			
+				self.number_list.append(0)
+						
+			else:
+				self.number_list.append(index_finder())
+				#last_occurance = len(number_list) -1- number_list[-2::-1].index(number_list[-1])	
+				#yield (len(number_list)-(last_occurance))
+				#number_list.append(len(number_list)-(last_occurance))
+			
+		print(self.number_list)
+
+	def index_finder(self):
+		last_occurance = len(self.number_list) -1- self.number_list[-2::-1].index(self.number_list[-1])	
+		return next(len(self.number_list)-(last_occurance))
+
 
 
 if __name__=="__main__":
-	recitation([10,16,6,0,1,17])
-	
+	big_gen.recitation([0,3,6])
