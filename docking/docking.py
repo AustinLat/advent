ba6696c818cabd7a6d42e80df456bebc7e752e18
Austin <alaturno@gmail.com>
2021-03-10 20:18:46 -0800

    starting day 14

---
+++ docking/docking.py
@@ -0,0 +1,15 @@
+def docking():
+#	with open("input.txt", "r") as r:
+#		data = r.read()
+#	print(data)
+
+	data = """mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0"""
+	print(data)
+
+
+
+if __name__=="__main__":
+	docking()	
